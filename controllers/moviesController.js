4cfbcef725bf6f6ef87ba9523fb65972352fb627
David Orona <OronaDavidJ@JohnDeere.com>
2020-11-02 10:58:37 -0600

    Make movie form for movies/add post

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -11,6 +11,12 @@ router.get("/add", (req, res) => {
     res.render("movies/add.ejs");
 });
 
+router.post('/add', (req, res) => {
+    Media.create(req.body).then((newMovie) => {
+        res.redirect(`/movies/${newMovie.id}`);
+    });
+});
+
 router.get("/:id", (req, res) => {
     res.render("movies/show.ejs");
 });
