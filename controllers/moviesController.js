734ae122f9611f99d43f676d340089288ad5d479
David Orona <OronaDavidJ@JohnDeere.com>
2020-11-05 08:08:06 -0600

    Remove API methods

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -27,69 +27,11 @@ router.get("/add", (req, res) => {
 
 router.post('/add', (req, res) => {
     User.findByPk(req.user.id).then((user) => {
-        // getTitleFromUPC(`${req.body.upc}`).then((title) => {
-            // getMovieInfoFromTitle(req.body.title).then((response) => {
-                // console.log(req.body);
-                // console.log('XXXXX');
-                // console.log(response && response.data.results[0].title === req.body.title);
-                // if (response && response.data.results[0].title === req.body.title) {
-                //     let tempDate = new Date(response.data.results[0].release_date)
-                //     req.body.year = `${tempDate.getFullYear()}`;
-                //     req.body.runTime = "0";
-                //     // req.body.description = response.data.results[0].overview;
-                // }
-                // console.log('YYYYY');
-                // console.log(req.body);
-                Movie.create(req.body).then((newMovie) => {
-                    user.addMovie(newMovie);
-                    res.redirect(`/movies/${newMovie.id}`);
-                });
-            // });
-        // });
+        Movie.create(req.body).then((newMovie) => {
+            user.addMovie(newMovie);
+            res.redirect(`/movies/${newMovie.id}`);
+        });
     });
-
-    // function getTitleFromUPC(upcNumber) {
-    //     return axios({
-    //         method: `get`,
-    //         url: 'https://api.barcodelookup.com/v2/products',
-    //         params: {
-    //             'barcode': upcNumber,
-    //             'key': process.env.UPC_API_KEY
-    //         }
-    //     })
-    //     .then(response => {
-    //         // console.log(response.data.products[0].product_name);
-    //         req.body.title = response.data.products[0].product_name;
-    //         console.log(req.body);
-    //     })
-    //     .catch(error => console.log(error));
-    // };
-
-    function getMovieInfoFromTitle(title) {
-        return axios({
-            method: `get`,
-            url: 'https://api.themoviedb.org/3/search/movie',
-            params: {
-                'api_key': process.env.MOVIEDB_API_KEY,
-                'language': 'en-US',
-                'query': title,
-                'page': '1',
-                'include_adult': 'false',
-            }
-        })
-        // .then(response => {
-        //     console.log('XXXXX');
-        //     console.log(response.data.results[0]);
-        //     if (response) {
-        //         req.body.title = response.data.results[0].title;
-        //         req.body.year = Date.parse(response.data.results[0].release_date).getYear();
-        //         req.body.runTime = 0;
-        //         req.body.description = response.data.results[0].overview;
-        //         console.log(req.body);
-        //     }
-        // })
-        // .catch(error => console.log(error));
-    };
 });
 
 router.get("/:id", (req, res) => {
